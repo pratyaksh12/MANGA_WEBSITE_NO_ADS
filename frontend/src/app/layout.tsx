51c953ad9855261647664ad300027ceb4a4f3fea
Pratyaksh <pratyakshbasel@gmail.com>
2025-12-19 00:09:50 +1100

    add-side-bar-with-cool-animation

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import StaggeredMenu, { StaggeredMenuItem } from "@/components/StaggeredMenu";
 
 export const metadata: Metadata = {
   title: "I'll see what I'll call it",
@@ -14,6 +15,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
+        <StaggeredMenu 
+          isFixed={true}
+          displayItemNumbering={false}
+          items={[
+            { label: "Home", link: "/", ariaLabel: "Go to Home" },
+            { label: "Popular", link: "/", ariaLabel: "View Popular" },
+            { label: "Bookmarks", link: "/bookmarks", ariaLabel: "View Bookmarks" },
+            { label: "History", link: "/history", ariaLabel: "View History" },
+          ]}
+        />
         {children}
       </body>
     </html>
